a390bf537004211a774969876a13237a2ecb0bc3
thebigG <yunior.eury@gmail.com>
2025-11-04 22:49:27 -0600

    -Add support for generating .gdshaderinc files -Use argparse for arguments

--- src/glsl_to_gd_converter/glsl_to_godot.py
+++ src/glsl_to_gd_converter/glsl_to_godot.py
@@ -1,6 +1,30 @@
 import re, sys, os
+import argparse
 
-def convert_glsl_to_godot(glsl_code, shader_type="canvas_item"):
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description="Convert GLSL shaders to Godot shader format."
+    )
+    parser.add_argument(
+        "input",
+        metavar="input.glsl",
+        help="Path to the input GLSL shader file."
+    )
+    parser.add_argument(
+        "shader_type",
+        nargs="?",
+        default=None,
+        choices=["canvas_item", "spatial", "sky", "particles"],
+        help="Type of Godot shader to generate (default: canvas_item)."
+    )
+
+    args = parser.parse_args()
+    return args
+
+
+
+def convert_glsl_to_godot(glsl_code, shader_type):
     # Remove GLSL version line
     code = re.sub(r"#version\s+\d+\s*\n", "", glsl_code)
 
@@ -25,26 +49,33 @@ def convert_glsl_to_godot(glsl_code, shader_type="canvas_item"):
     # Remove 'in' and 'out' qualifiers (Godot uses 'varying')
     code = re.sub(r"\b(in|out)\b\s+(\w+)\s+", r"varying \2 ", code)
 
-    # Add shader_type header
-    header = f"shader_type {shader_type};\n\n"
-    return header + code.strip() + "\n"
+    # Add shader_type header if specified
+    if shader_type is None:
+        return code.strip() + "\n"
+    else:
+        header = f"shader_type {shader_type};\n\n"
+        return header + code.strip() + "\n"
 
 
 def main():
-    if len(sys.argv) < 2:
-        print("Usage: glsl_to_godot.py <input.glsl> [shader_type]")
-        print("shader_type: canvas_item | spatial | sky | particles")
-        sys.exit(1)
 
-    infile = sys.argv[1]
-    shader_type = sys.argv[2] if len(sys.argv) > 2 else "canvas_item"
+    args = parse_args()
+
+    infile = args.input
+    shader_type = args.shader_type
 
     with open(infile, "r") as f:
         glsl_code = f.read()
 
     godot_code = convert_glsl_to_godot(glsl_code, shader_type)
 
-    outfile = os.path.splitext(infile)[0] + ".gdshader"
+    # If no shader type specified, save as .gdshaderinc
+    # As per Godot conventions for includes without shader_type (https://docs.godotengine.org/en/stable/tutorials/shaders/shader_reference/shader_preprocessor.html#include)
+    if shader_type is None:
+        outfile = os.path.splitext(infile)[0] + ".gdshaderinc"
+    else:
+        outfile = os.path.splitext(infile)[0] + f"_{shader_type}.gdshader"
+
     with open(outfile, "w") as f:
         f.write(godot_code)
 
